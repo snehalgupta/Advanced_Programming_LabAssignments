53fa8eff817e4aa4b87dd97d4e3fa40ae5e05272
snehalgupta <snehal16201@iiitd.ac.in>
2018-02-26 23:29:38 +0530

    final commit

---
+++ Questions/lab5/lab5/BSTFilesBuilder.java
@@ -0,0 +1,45 @@
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class BSTFilesBuilder {
+
+	public void createBSTFiles(int numStudents, int numTrees) {
+		Random rand = new Random();
+		for (int i = 1; i <= numTrees; i++) {
+		    try {
+				PrintWriter w = new PrintWriter("./src/" + i + ".txt", "UTF-8");
+				int type = rand.nextInt(3) + 1;
+				if(type == 1) {
+					w.println("Integer");
+					w.println(numStudents);
+					for (int j = 1; j <= numStudents; j++) {
+						w.print(rand.nextInt(1000));
+						w.print(" ");
+					}
+				}
+				else if(type == 2) {
+					w.println("Float");
+					w.println(numStudents);
+					for (int j = 1; j <= numStudents; j++) {
+						w.print(rand.nextFloat()*1000);
+						w.print(" ");
+					}
+				}
+				else {
+					w.println("String");
+					w.println(numStudents);
+					String alphabet = "abcdefghijklmnopqrstuvwxyz";
+					for (int j = 1; j <= numStudents; j++) {
+						int len = rand.nextInt(10)+1;
+						for (int k = 0; k < len; k++)
+							w.print(alphabet.charAt(rand.nextInt(alphabet.length())));
+						w.print(" ");
+					}
+				}
+				w.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
